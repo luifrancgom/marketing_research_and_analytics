fd02fee01ea67ef2070b0026e87ae741ff8d5e34
luifrancgom <luifrancgom@gmail.com>
2024-04-19 20:28:40 -0500

    Adding the final report in relation to the first part of the course

--- 003_marketing_report_1/001_marketing_report_part1_script.R
+++ 003_marketing_report_1/001_marketing_report_part1_script.R
@@ -41,11 +41,52 @@ bike_sales |>
 
 # Checking revenue by customer
 bike_sales_by_bikeshop_revenue <- bike_sales |> 
-  mutate(revenue = price * quantity) |> 
-  select(bikeshop.name, revenue) |> 
+  select(bikeshop.name, price.ext) |> 
   group_by(bikeshop.name) |> 
-  summarise(total_revenue = sum(revenue)) |> 
+  summarise(total_revenue = sum(price.ext)) |> 
   arrange(desc(total_revenue))
 
+bike_sales_by_bikeshop_revenue |> 
+  head()
+
+bike_sales_by_bikeshop_revenue |> 
+  tail()
+
 bike_sales_by_bikeshop_revenue
-  
+
+# Checking revenue by second category
+bike_sales_by_bikeshop_second_category <- bike_sales |> 
+  select(category.secondary, price.ext) |> 
+  group_by(category.secondary) |> 
+  summarise(total_revenue = sum(price.ext)) |> 
+  arrange(desc(total_revenue))
+
+bike_sales_by_bikeshop_second_category
+
+# visualization ----
+bike_sales |> 
+  ggplot() +
+  geom_boxplot(aes(x = price.ext, 
+                   y = category.secondary)) +
+  # scale_x_continuous(breaks = seq.int(from = 0, to = 10000000, by = 1000000)) +
+  labs(x = "Revenue by transaction (Dollars)",
+       y = "Bike models by second category",
+       title = "Distribution of revenue by second category")
+
+bike_sales |> 
+  ggplot() +
+  geom_boxplot(aes(x = price.ext, 
+                   y = frame)) +
+  scale_x_continuous(labels = scales::label_dollar()) +
+  labs(x = "Revenue by transaction (Dollars)",
+       y = "Bike models by frame",
+       title = "Distribution of revenue by frame")  
+
+
+bike_sales_by_bikeshop_revenue |> 
+  mutate(bikeshop.name = fct_reorder(bikeshop.name, 
+                                     total_revenue)) |> 
+  ggplot() +
+  geom_col(aes(x = total_revenue, 
+               y = bikeshop.name))
+
