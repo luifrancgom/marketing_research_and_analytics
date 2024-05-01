8b162fb1da8e8208d07edfe9de690190bb1f0791
luifrancgom <luifrancgom@gmail.com>
2024-05-01 15:40:36 -0500

    Adding files related to session 1 chapter 5

---
+++ 005_comparing_groups_tables_visualizations/001_comparing_groups_tables_visualizations_script.R
@@ -0,0 +1,6 @@
+# Libraries ----
+library(tidyverse)
+
+# Import data ----
+# segmentation <- read_csv(file = "http://goo.gl/qw303p")
+segmentation <- read_csv(file = "000_data/005_data_chapter5.csv")
