cec5c74e1121dce92b93746d4c03801823c86321
luifrancgom <luifrancgom@gmail.com>
2024-05-03 20:30:02 -0500

    Adding code from the script session 2 chapter 5

--- 005_comparing_groups_tables_visualizations/001_comparing_groups_tables_visualizations_script.R
+++ 005_comparing_groups_tables_visualizations/001_comparing_groups_tables_visualizations_script.R
@@ -61,3 +61,81 @@ subscriber_by_segment_home_ownership |>
   geom_col(aes(x = subscribe, y = percentage_n)) +
   facet_wrap(facets = vars(Segment, ownHome),
              nrow = 2, ncol = 4)
+
+subscriber_by_segment_home_ownership |> 
+  ggplot() + 
+  geom_col(aes(x = subscribe, y = percentage_n)) +
+                       # Notation formula
+  facet_wrap(facets =  ~ Segment + ownHome,
+             nrow = 2, ncol = 4)
+
+# Knowing the percentage of clients by segment ---
+## Prepare data
+prop_table <- segmentation |> 
+  count(subscribe, Segment) |> 
+  group_by(Segment) |> 
+  mutate(n_pct = (n / sum(n)) * 100) |> 
+  filter(subscribe == "subYes")
+
+## Visualization
+prop_table |> 
+  ggplot() +
+  geom_col(aes(x = n_pct, 
+               y = fct_reorder(Segment, n_pct)),
+           # Selecting specific colors
+           ## Hex colors
+           ### https://color-hex.org/
+           color = "black", fill = "#a3b8c8") +
+  labs(x = "Percentage (%)",
+       y = "",
+       title = "Percentage of subscribers by segment")
+
+# Knowing mean income by segment 
+# and if you own a home
+
+## Prepare
+seg_income_ag <- segmentation |> 
+  group_by(Segment, ownHome) |> 
+  summarise(mean_income = mean(income)) |> 
+  arrange(desc(mean_income))
+
+## Visualize
+seg_income_ag |> 
+  ggplot() +
+  geom_col(aes(x = mean_income, 
+               y = fct_reorder(Segment, 
+                               mean_income),
+               fill = ownHome),
+           position = position_dodge(),
+           color = "black") +
+  scale_fill_manual(values = c("#123782", "#d6912d"))
+# Add labels with labs(x = , y = , title)
+
+# Distribution of mean income by segment
+# and own Home
+seg_income_ag |> 
+  ggplot() +
+  geom_boxplot(aes(x = mean_income, 
+                   y = Segment)) +
+  facet_wrap(facets = vars(ownHome))
+
+## With formula notation
+seg_income_ag |> 
+  ggplot() +
+  geom_boxplot(aes(x = mean_income, 
+                   y = Segment)) +
+  facet_wrap(facets = ~ ownHome)
+
+# Distribution of income by segment
+# and own Home
+segmentation |> 
+  ggplot() +
+  geom_boxplot(aes(x = income, 
+                   y = fct_reorder(Segment,
+                                   income)),
+               fill = "#d7b4ae") +
+  facet_wrap(facets = ~ ownHome) +
+  labs(x = "Income (US dollars)",
+       y = "",
+       title = "Distribution of income by Segment and own home")
+
