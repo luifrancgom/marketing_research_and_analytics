0bc3c21e6c25e65920366423f93d0a20e23d2f0e
luifrancgom <luifrancgom@gmail.com>
2024-05-15 16:19:59 -0500

    Adding template for session 1 chapter 7

---
+++ 007_identifying_drivers_of_outcomes_linear_models/007_identifying_drivers_of_outcomes_linear_models_script.R
@@ -0,0 +1,10 @@
+# Libraries ----
+library(tidyverse)
+
+# Import data ----
+## https://r-marketing.r-forge.r-project.org/ >
+## Data >
+## folder of all data files >
+## rintro-chapter7.csv
+# amusement_park <- read_csv("000_data/007_data_chapter7.csv")
+amusement_park <- read_csv("http://goo.gl/HKnl74")
