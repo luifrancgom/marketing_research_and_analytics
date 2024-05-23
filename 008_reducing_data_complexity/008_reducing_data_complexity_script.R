c1f99dbc61d5f1cd97de283c3b22a8c9bdc53672
luifrancgom <luifrancgom@gmail.com>
2024-05-22 20:50:09 -0500

    Adding work related to session 1 Chapter 8

--- 008_reducing_data_complexity/008_reducing_data_complexity_script.R
+++ 008_reducing_data_complexity/008_reducing_data_complexity_script.R
@@ -1,7 +1,9 @@
 # Libraries ----
 # library(imager)
 library(tidyverse)
-
+library(skimr)
+library(corrr)
+library(tidyheatmaps)
 
 # Import data ----
 # consumer_brand <- read_csv("http://goo.gl/IQl8nc")
@@ -11,7 +13,80 @@ consumer_brand <- read_csv("000_data/008_data_chapter8.csv")
 consumer_brand |> glimpse()
 
 # Transform data ----
+## Standarizing
+1:10
+mean(x = 1:10)
+sd(x = 1:10)
 
+1:10 - mean(x = 1:10)
+(1:10 - mean(x = 1:10)) |> mean()
+
+(1:10 - mean(x = 1:10)) / sd(x = 1:10)
+
+(1000:1010 - mean(x = 1000:1010)) / sd(x = 1000:1010)
+
+scale(x = 1:10, center = TRUE, scale = TRUE)[, 1]
+
+## Not efficient
+consumer_brand |> 
+  mutate(perform = scale(x = perform, 
+                         center = TRUE, 
+                         scale = TRUE)[,1],
+         leader = scale(x = leader, 
+                         center = TRUE, 
+                         scale = TRUE)[,1])
+
+# Efficient
+consumer_brand_scale <- consumer_brand |> 
+  mutate(across(.cols = perform:rebuy,
+                .fns = ~ scale(x = .x,
+                               center = TRUE,
+                               scale = TRUE)[,1]))
+
+consumer_brand_scale
+
+# Summarize data ----
+consumer_brand_scale |> skim()
+
+# Calculating correlations ----
+correlation_matrix <- consumer_brand_scale |> 
+  select(perform:rebuy) |> 
+  correlate(method = "pearson",
+            use = "pairwise.complete.obs")
+
+correlation_matrix
+
+correlation_matrix |> 
+  autoplot(triangular = "lower",
+           method = "HC")
+
+# Using the mean
+brand_mean <- consumer_brand_scale |> 
+  group_by(brand) |> 
+  summarise(across(.cols = perform:trendy,
+                   .fns = ~ mean(x = .x)))
+
+brand_mean <- consumer_brand_scale |> 
+  group_by(brand) |> 
+  summarise(across(.cols = perform:trendy,
+                   .fns = mean))
+
+brand_mean
+
+brand_mean_long <- brand_mean |> 
+  pivot_longer(cols = perform:trendy, 
+               names_to = "perceptual_adjective",
+               values_to = "value_mean")
+
+tidyheatmap(df = brand_mean_long,
+            rows = brand,
+            columns = perceptual_adjective,
+            values = value_mean,
+            border_color = "black",
+            display_numbers = TRUE,
+            clustering_method = "complete",
+            cluster_rows = TRUE, 
+            cluster_cols = TRUE)
 
 # Using images to understand data complexity ----
 
