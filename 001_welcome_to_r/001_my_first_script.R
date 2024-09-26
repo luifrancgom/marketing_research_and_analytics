4da1649cc03753a9f4506e75ee4f48cd13b53758
luifrancgom <luifrancgom@gmail.com>
2024-09-25 20:41:18 -0500

    Adding session 1 from chapter 1

---
+++ 001_welcome_to_r/001_my_first_script.R
@@ -0,0 +1,16 @@
+# Packages
+# Opening the packages
+library(tidyverse)
+
+# Error
+# Naming the generates possible
+# conflicts
+x <- c(1, 2, 3, 4, 5)
+print(x)
+X
+
+my_vector <- c(6, 7, 8, 9)
+my_vector
+
+# Warning
+sqrt(-9)
