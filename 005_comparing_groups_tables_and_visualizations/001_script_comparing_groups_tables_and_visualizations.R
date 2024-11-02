23b269ec65fd838fd0fd67e98519676c8ca55071
luifrancgom <luifrancgom@gmail.com>
2024-11-01 21:05:40 -0500

    Adding chapter 5 session 2 and progress Marketing Report 2

--- 005_comparing_groups_tables_and_visualizations/001_script_comparing_groups_tables_and_visualizations.R
+++ 005_comparing_groups_tables_and_visualizations/001_script_comparing_groups_tables_and_visualizations.R
@@ -1,6 +1,8 @@
 # Libraries ----
 library(tidyverse)
 library(skimr)
+library(scales)
+
 
 # Import data ----
 segmentation <- read_csv(file = "000_data/005_data_chapter5.csv")
@@ -81,6 +83,7 @@ df_sorted <- df |>
   arrange(age)
 
 ## Visualization by groups ----
+### Using facets ----
 subscriber_by_segment_home_ownership |> 
   ggplot() +
   geom_col(aes(x = subscribe, 
@@ -88,3 +91,82 @@ subscriber_by_segment_home_ownership |>
   facet_wrap(facets = vars(ownHome, 
                            Segment), 
              nrow = 2, ncol = 4)
+
+### Plotting proportions ----
+#### Question 4: What proportion of clients 
+####             are subscribers by segment?
+prop_table <- segmentation |> 
+  select(Segment, subscribe) |> 
+  count(Segment, subscribe) |> 
+  group_by(Segment) |> 
+  mutate(pct_subscribers = n / sum(n)) |> 
+  ungroup() |> 
+  filter(subscribe == "subYes") |> 
+  mutate(Segment = fct_reorder(.f = Segment,
+                               .x = pct_subscribers))
+
+prop_table |> 
+  ggplot() + 
+  geom_col(aes(x = pct_subscribers, 
+               y = Segment),
+           color = "black",
+           fill = "#7ED4AD") + 
+  labs(x = "Proportion of subscribers",
+       y = "",
+       title = "Proportions of subscribers by segment")
+
+### Grouping with a continuous variable ----
+#### Question 5: Are there differences between 
+####             multiple categories(Segment, ownHome)
+####             in relation to income
+
+# Note: another option using the argument
+# .group = "drop"
+segmentation |> 
+  select(income, ownHome, Segment) |> 
+  group_by(Segment, ownHome) |> 
+  summarise(mean_income = mean(income), 
+            .groups = "drop")
+
+mean_income_by_seg_ownhome <- segmentation |> 
+  select(income, ownHome, Segment) |> 
+  group_by(Segment, ownHome) |> 
+  summarise(mean_income = mean(income)) |> 
+  ungroup()
+
+mean_income_by_seg_ownhome |> 
+  ggplot() + 
+  geom_col(aes(x = Segment, y = mean_income,
+               fill = ownHome),
+           position = position_dodge(),
+           color = "black") + 
+  scale_fill_manual(values = c("#7ED4AD", 
+                               "#D76C82")) +
+  labs(x = "",
+       y = "Mean disposable income (US Dollars)",
+       fill = "Owning home status",
+       title = "Comparing of mean income by segment and owning home status")
+
+### Grouping with a continuous variable and using facets ----  
+#### Question 6: Distribution of income by more than
+####             2 categories (ownHome, Segment)
+income_distribution_by_seg_ownhome <- segmentation |> 
+  select(income, ownHome, Segment) |>
+  mutate(Segment = fct_reorder(.f = Segment,
+                               .x = income))
+  
+income_distribution_by_seg_ownhome |>    
+  ggplot() + 
+  geom_boxplot(aes(x = income, y = Segment,
+                   fill = ownHome)) + 
+  scale_fill_manual(values = c("#7ED4AD", 
+                               "#D76C82")) +
+  scale_x_continuous(labels = label_dollar(scale = 0.01)) +
+  facet_wrap(facets = vars(ownHome)) + 
+  labs(x = "Disposable income in thousands (USD dollars)",
+       y = "",
+       fill = "Owning home status",
+       title = "Distribution of income by owning home status and segment")
+  
+
+
--- 006_marketing_report2/001_script_marketing_report_part-2.R
+++ 006_marketing_report2/001_script_marketing_report_part-2.R
@@ -1,6 +1,7 @@
 # Libraries ----
 library(sweep)
 library(tidyverse)
+library(scales)
 
 # Import data ----
 bike_sales <- bike_sales
@@ -9,4 +10,84 @@ bike_sales <- bike_sales
 bike_sales |> glimpse()
 
 # Transform data ----
+bike_sales <- bike_sales |> 
+  mutate(order.id = factor(x = order.id, 
+                           ordered = FALSE),
+         order.line = factor(x = order.id, 
+                             ordered = TRUE),
+         customer.id = factor(x = customer.id, 
+                              ordered = FALSE),
+         bikeshop.name = factor(x = bikeshop.name, 
+                                ordered = FALSE),
+         bikeshop.city = factor(x = bikeshop.city, 
+                                ordered = FALSE),
+         product.id = factor(x = product.id, 
+                                ordered = FALSE),
+         category.primary = factor(x = category.primary , 
+                                   ordered = FALSE),
+         category.secondary = factor(x = category.secondary, 
+                                     ordered = FALSE),
+         frame = factor(x = frame, 
+                        ordered = FALSE))
+
+# Data visualization ----
+## Price vs quantity ----
+bike_sales_p_q_frame <- bike_sales |> 
+  select(price, quantity, frame)
+
+### Without transformation ----
+bike_sales_p_q_frame |> 
+  ggplot() + 
+  geom_point(aes(x = price, y = quantity,
+                 color = frame)) +
+  scale_x_continuous(labels = label_dollar()) +
+  labs(x = "Price (US Dollars)",
+       y = "Quantity (units)",
+       color = "Frame",
+       title = "Relation between price and units sale")
+
+### Experiment ----  
+# Note: checking if by facetting we ge something new
+## Definitely no!!!
+bike_sales_p_q_frame_cat2 <- bike_sales |> 
+  select(price, quantity, frame, category.secondary)
+
+bike_sales_p_q_frame_cat2 |> 
+  ggplot() + 
+  geom_point(aes(x = price, y = quantity,
+                 color = frame)) +
+  scale_x_continuous(labels = label_dollar()) +
+  facet_wrap(facets = vars(category.secondary)) +
+  labs(x = "Price (US Dollars)",
+       y = "Quantity (units)",
+       color = "Frame",
+       title = "Relation between price and units sale")
+
+### With logarithmic transformation
+bike_sales_p_q_frame |> 
+  ggplot() + 
+  geom_point(aes(x = price, y = quantity,
+                 color = frame)) +
+  scale_x_continuous(labels = label_dollar(), 
+                     transform = "log10") +
+  scale_y_continuous(transform = "log10") +
+  labs(x = "Price (US Dollars) - Logarithmic Base 10 scale",
+       y = "Quantity (units)  - Logarithmic Base 10 scale",
+       color = "Frame",
+       title = "Relation between price and units sale")
+
+## Frame vs Second category ----
+bike_sales_frame_cat2 <- bike_sales |> 
+  select(frame, category.secondary)
+
+bike_sales_frame_cat2 |> 
+  ggplot() +
+  geom_point(aes(x = frame, y = category.secondary),
+             position = position_jitter(width = 0.2,
+                                        height = 0.2)) +
+  labs(x = "Frame material",
+       y = "Secondary Category",
+       title = "Identifying product gaps")
+
+  
 
