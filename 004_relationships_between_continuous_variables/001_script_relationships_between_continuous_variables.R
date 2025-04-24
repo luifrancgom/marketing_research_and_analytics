fe3e5ac051c993d6696cbd7a460b1c8d7a6e8d2f
luifrancgom <luifrancgom@gmail.com>
2025-04-23 21:11:31 -0500

    Adding code session 1 chapter 4

--- 004_relationships_between_continuous_variables/001_script_relationships_between_continuous_variables.R
+++ 004_relationships_between_continuous_variables/001_script_relationships_between_continuous_variables.R
@@ -27,7 +27,49 @@ customer
 
 # Summarize ----
 customer |> 
+  select(-c(cust.id)) |> 
   skim()
 
+# Visualization ----
+## Spending ----
+customer |> 
+  ggplot() +
+  geom_point(aes(x = online.spend, 
+                 y = store.spend))
 
+customer |> 
+  ggplot() + 
+  geom_point(aes(x = online.spend,
+                 y = store.spend,
+                 color = email)) +
+  labs(x = "Amount spend online yearly (US dollars)",
+       y = "Amount spend in the physical store (US dollars)",
+       color = "Email",
+       title = "Spend online vs physical store by customer")
 
+customer |> 
+  ggplot() + 
+  geom_point(aes(x = online.spend,
+                 y = store.spend,
+                 color = email)) +
+  scale_color_manual(values = c("#8e0c14", "#2c98af"),
+                     labels = c("No", "Yes")) +
+  labs(x = "Amount spend online yearly (US dollars)",
+       y = "Amount spend in the physical store (US dollars)",
+       color = "Email",
+       title = "Spend online vs physical store by customer")
+
+# Satisfaction ----
+customer |> 
+  ggplot() +
+  geom_point(aes(x = sat.service, y = sat.selection),
+             position = position_jitter(width = 0.2,
+                                        height = 0.2))
+
+customer |> 
+  count(sat.service, sat.selection) |> 
+  ggplot() +
+  geom_label(aes(x = sat.service,
+                 y = sat.selection,
+                 label = n))
+  
