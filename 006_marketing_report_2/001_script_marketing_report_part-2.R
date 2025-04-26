dbab493fd1680ba621394263608b809daf082598
luifrancgom <luifrancgom@gmail.com>
2025-04-25 21:22:22 -0500

    Adding script session 2 chapter 2

--- 006_marketing_report_2/001_script_marketing_report_part-2.R
+++ 006_marketing_report_2/001_script_marketing_report_part-2.R
@@ -34,5 +34,47 @@ bike_sales <- bike_sales |>
          frame = factor(x = frame, 
                         ordered = FALSE))
 
+# Relation between continuous variables ----
+## price vs quantity (color by frame)
+bike_sales |> 
+  ggplot() +
+  geom_point(aes(x = quantity, 
+                 y = price,
+                 color = frame)) +
+  labs(x = "Quantity",
+       y = "Price (per unit in US Dollars)",
+       color = "Frame",
+       title = "Quantities sold vs price of frames")
 
+## price vs quantity (color by frame) transformed
+bike_sales |> 
+  ggplot() +
+  geom_point(aes(x = quantity, 
+                 y = price,
+                 color = frame)) +
+  scale_x_continuous(transform = "log") +
+  scale_y_continuous(transform = "log") +
+  labs(x = "Quantity",
+       y = "Price (per unit in US Dollars)",
+       color = "Frame",
+       title = "Quantities sold vs price of frames")
+
+## Summary statistics ----
+corr_price_quantity <- bike_sales |> 
+  summarise(corr_price_quantity = cor(quantity, price))
+
+# Product gap ----
+bike_sales |> 
+  ggplot() + 
+  geom_point(aes(x = frame, y = category.secondary),
+             position = position_jitter(width = 0.2,
+                                        height = 0.2))
+
+bike_sales |> 
+  count(frame, category.secondary) |> 
+  ggplot() +
+  geom_label(aes(x = frame,
+                 y = category.secondary,
+                 label = n))
+  
 
