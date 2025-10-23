4670e8badc252ec7504d49d4dbbb507731274002
luifrancgom <luifrancgom@gmail.com>
2025-10-22 21:05:02 -0500

    adding script session 1 chapter 4

--- 004_relationships_between_continuous_variables/001_relationships_between_continuous_variables_script.R
+++ 004_relationships_between_continuous_variables/001_relationships_between_continuous_variables_script.R
@@ -1,6 +1,8 @@
 # Libraries ----
 library(tidyverse)
 library(skimr)
+library(corrr)
+library(DT)
 
 # Import ----
 # customer <- read_csv(file = "http://goo.gl/PmPkaG")
@@ -28,3 +30,54 @@ customer |>
 customer |> 
   skim()
   
+# Visualization ----
+customer |> 
+  ggplot() +
+  geom_point(aes(x = store.spend,
+                 y = online.spend,
+                 colour = email)) +
+  scale_x_continuous(transform = "log1p",
+                     breaks = c(1, 2, 5, 
+                                10, 20, 50, 
+                                100, 500)) + 
+  scale_y_continuous(transform = "log1p",
+                     breaks = c(1, 5, 
+                                50, 500))
+  
+# Correlation
+## 2 variables
+customer$store.spend
+customer$online.spend
+
+cor(x = customer$store.spend,
+    y = customer$online.spend, 
+    method = "pearson")
+
+## Multiple variables
+correlation_matrix <- customer |> 
+  select(where(is.numeric)) |> 
+  correlate(use = "pairwise.complete.obs")
+
+correlation_matrix |> 
+  datatable()
+
+## Visualization
+correlation_matrix |> 
+  autoplot(triangular = "lower")
+
+# Categorical variables
+customer |> 
+  ggplot() +
+  geom_point(aes(x = sat.service,
+                 y = sat.selection),
+             position = position_jitter(width = 0.2,
+                                        height = 0.2))
+customer |> 
+  count(sat.service,
+        sat.selection) |> 
+  ggplot() +
+  geom_label(aes(x = sat.service,
+                 y = sat.selection,
+                 label = n))
+  
+
