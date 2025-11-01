b03f75d0bf5cccdde1398ede8e38e8dc7fd99efc
luifrancgom <luifrancgom@gmail.com>
2025-10-31 20:11:08 -0500

    Marketing report 2 progress summary statistics by group

--- 006_marketing_report_2/001_marketing_report_part-2_script.R
+++ 006_marketing_report_2/001_marketing_report_part-2_script.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(sweep)
 library(skimr)
 library(scales)
+library(DT)
 
 # Import ----
 bike_sales <- bike_sales |> 
@@ -100,5 +101,93 @@ bike_sales |>
   labs(x = "Frame",
        y = "Category secondary",
        subtitle = "Frame vs Category secondary")
+
+# Descriptive statistics by group ----
+## Category secondary, frame ----
+### Mean, median, sd ----
+bike_sales_cat2_frame_stats_rev <- bike_sales |> 
+  group_by(category.secondary, frame) |> 
+  summarise(
+    mean_rev = mean(x = price.ext),
+    median_rev = median(x = price.ext),
+    sd_rev = sd(x = price.ext)
+  ) |> 
+  ungroup()
+
+bike_sales_cat2_frame_stats_rev |> 
+  datatable(
+    colnames = c("Category secondary",
+                 "Frame",
+                 "Mean revenue",
+                 "Median revenue",
+                 "Standard deviation revenue") 
+      ) |> 
+  formatRound(
+    columns = c("mean_rev",
+                "sd_rev"), 
+    digits = 2  
+  )
+  
+### Total and percentage revenue ----
+bike_sales_cat2_frame_total_pct_rev <- bike_sales |> 
+  group_by(category.secondary, frame) |> 
+  summarise(total_rev = sum(price.ext)) |> 
+  ungroup() |> 
+  mutate(total_rev_pct = (total_rev / sum(total_rev)) * 100)
+  
+bike_sales_cat2_frame_total_pct_rev |> 
+  datatable(
+    colnames = c("Category secondary",
+                 "Frame",
+                 "Total revenue",
+                 "Percentage of total revenue") 
+  ) |> 
+  formatRound(
+    columns = c("total_rev_pct") ,
+    digits = 2
+      )
+
+## Bikeshop ---- 
+### Mean, median, sd ----
+bike_sales_bikeshop_stats_rev <- bike_sales |> 
+  group_by(bikeshop.name) |> 
+  summarise(
+    mean_rev = mean(x = price.ext),
+    median_rev = median(x = price.ext),
+    sd_rev = sd(x = price.ext)
+  )
+
+bike_sales_bikeshop_stats_rev |> 
+  datatable(
+    colnames = c("Bikeshop",
+                 "Mean revenue",
+                 "Median revenue",
+                 "Standard deviation revenue")
+  ) |> 
+  formatRound(
+    columns = c("mean_rev",
+                "sd_rev"),
+    digits = 2
+  )
+
+### Total and percentage revenue ----
+bike_sales_bikeshop_total_rev_pct <- bike_sales |> 
+  group_by(bikeshop.name) |> 
+  summarise(total_rev = sum(price.ext)) |> 
+  mutate(total_rev_pct = (total_rev / sum(total_rev)) * 100)
+
+bike_sales_bikeshop_total_rev_pct |> 
+  datatable(
+    colnames = c("Bikeshop",
+                 "Total revenue",
+                 "Percentage of total revenue")
+  ) |> 
+  formatRound(
+    columns = c("total_rev_pct"),
+    digits = 2
+  )
+  
+
+
 
 
