cc2c2d88bfb79e80335e723626d587bdd087bde9
luifrancgom <luifrancgom@gmail.com>
2025-11-19 21:18:52 -0500

    add session 1 chapter 8 pca

---
+++ 008_reducing_data_complexity/008_reducing_data_complexity_script.R
@@ -0,0 +1,121 @@
+# Libraries ----
+library(tidyverse)
+library(skimr)
+library(corrr)
+library(tidyheatmaps)
+# If you want to use it
+# install.packages("imager")
+library(imager)
+
+# Import ----
+# consumer_brand <- read_csv(file = "http://goo.gl/IQl8nc")
+consumer_brand <- read_csv(file = "000_data/008_chapter_8.csv")
+
+# Inspect ----
+consumer_brand |> 
+  glimpse()
+
+# Count ----
+consumer_brand |> 
+  count(brand)
+
+consumer_brand |> 
+  count(perform, brand)
+
+# Summarise data ----
+consumer_brand |> 
+  skim()
+
+# Scale data
+?scale
+
+tibble(x = 1:10,
+       y = (1:10)*100)
+
+(1:10 - mean(1:10)) / sd(1:10)
+((1:10)*100 - mean((1:10)*100)) / sd((1:10)*100)
+
+scale(x = 1:10,
+      center = TRUE,
+      scale = TRUE)[,1]
+
+scale(x = (1:10)*100,
+      center = TRUE,
+      scale = TRUE)[,1]
+
+function_add_2 <- \(x) x + 2
+function_add_2(x = 100)
+
+consumer_brand_scale <- consumer_brand |> 
+  mutate(across(.cols = perform:rebuy, 
+                .fns = \(x) scale(x = x,
+                                  center = TRUE,
+                                  scale = TRUE)[,1]))
+
+# Correlation matrix ----
+correlation_matrix <- consumer_brand_scale |> 
+  select(perform:rebuy) |> 
+  corrr::correlate(
+    method = "pearson", 
+    use = "pairwise.complete.obs"
+    )
+
+correlation_matrix |> 
+  autoplot(triangular = "lower", 
+           method = "HC")
+
+# Heatmaps ----
+brand_mean <- consumer_brand_scale |> 
+  group_by(brand) |> 
+  summarise(
+    across(.cols = perform:rebuy, 
+           .fns = mean)
+  )
+
+brand_mean_longer <- brand_mean |> 
+  pivot_longer(
+    cols = perform:rebuy, 
+    names_to = "perceptual_adjectives",
+    values_to = "value_mean"
+  )
+
+brand_mean_longer |> 
+  tidyheatmap(
+    rows = brand,
+    columns = perceptual_adjectives, 
+    values = value_mean, 
+    display_numbers = TRUE,
+    border_color = "black",
+    fontsize = 12,
+    cluster_rows = TRUE,
+    cluster_cols = TRUE,
+    clustering_method = "complete"
+  )
+
+# Principal component analysis (PCA) ----
+## Example ----
+set.seed(seed = 1234)
+consumer_brand_sample <- consumer_brand |>
+  slice_sample(n = 1, by = brand) |> 
+  select(brand, perform, leader)
+
+consumer_brand_sample
+
+# Applying PCA ----
+boat_gray <- load.image(file = "000_images/008_boat_gray_512_x_512.tiff") |> 
+  plot()
+
+boat_gray_long <- boat_gray |> 
+  as.data.frame() |> 
+  as_tibble()
+  
+boat_gray_wide <- boat_gray_long |> 
+  pivot_wider(
+    id_cols = x, 
+    names_from = y, 
+    values_from = value)
+  
+
+
+
+
